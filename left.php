8ce86eee9ae0735c7bad87b07b95077320fc09ef
IDEA ESB <ideaesb@gmail.com>
2012-09-24 08:56:12 -1000

    adjust piko definition div height

--- left.php
+++ left.php
@@ -15,7 +15,7 @@
 
   <div id ="pikoDefinition">
  
-   <b>nexus ['n&#603;ks&#601;s]<i> n pl nexus</i></b><br/><br/>
+   <b>nexus ['n&#603;ks&#601;s]<i> n pl nexus</i></b><br/>
    <p>1. a means of connection between members of a group or things in a series; link; bond</p>
    <p>2. a connected group or series [from Latin: a binding together, from nectere to bind]</p>
 
